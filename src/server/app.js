fa9f53fb35fbc14f17f51e568cd9499bcaff5c61
Alex Chen <achen19@mit.edu>
2019-01-02 09:32:50 -0800

    first webpack configs

---
+++ src/server/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const publicPath = path.resolve(__dirname, '..', '..', 'public');
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+  console.log(`Listening on port 3000 and looking in folder ${publicPath}`);
+});
