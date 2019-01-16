e62bacb2bc3a6ac79d2b818ddfd429a92dfff5e9
Shannen Wu <shannen@mit.edu>
2019-01-15 19:33:12 -0500

    dummy home page added

--- client/src/components/GameContainer.js
+++ client/src/components/GameContainer.js
@@ -1,14 +1,27 @@
 import React from "react";
 import GameBoard from "./game/GameBoard";
+import HomePage from "./HomePage";
 
 export default class GameContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      gameStatus: 0,
+    };
   }
 
   render() {
-    return (
-      <GameBoard />
-    );
+    switch (this.state.gameStatus) {
+      case 0:
+        return (
+          <HomePage />
+        );
+      case 1:
+        return (
+          <GameBoard />
+        );
+    }
   }
+
+
 }
