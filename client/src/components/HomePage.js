2ebac4b937477caf35586e80eac03424200ea2a0
Shannen Wu <shannen@mit.edu>
2019-01-16 01:09:01 -0500

    skeleton code for HomePage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,8 +7,7 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div className="center">
-        <GameTitle />
-        <div className="button" onClick={this.props.onClickStart}>Start</div>
+      
       </div>
     );
   }
