1a50a7633fd6564fba8f67305d9618fff37b93ee
Alex Chen <achen19@mit.edu>
2019-01-16 17:59:00 -0500

    homepage link

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../css/homepage.css";
 import "../css/app.css";
 import GameTitle from "./GameTitle";
+import Link from "react-router-dom/es/Link";
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <div className="center">
         <GameTitle />
+        <Link to="/rules" className={"button"}>Rules</Link>
         <div className="button" onClick={this.props.onClickStart}>Start</div>
       </div>
     );
