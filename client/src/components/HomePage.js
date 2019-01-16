98a51803975bba13aa25e586c5815a27895e7d94
Shannen Wu <shannen@mit.edu>
2019-01-16 11:39:31 -0500

    my name is home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "../css/homepage.css";
 import "../css/app.css";
-import GameTitle from "./GameTitle";
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <div className="center">
-      
+        MY NAME IS HOME PAGE
       </div>
     );
   }
