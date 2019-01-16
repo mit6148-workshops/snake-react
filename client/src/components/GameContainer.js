a674ba33773e8f8bae2320cdabe16fff2df43be2
Shannen Wu <shannen@mit.edu>
2019-01-16 01:05:13 -0500

    import homepage

--- client/src/components/GameContainer.js
+++ client/src/components/GameContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GameBoard from "./game/GameBoard";
+import HomePage from "./HomePage";
 
 export default class GameContainer extends React.Component {
   constructor(props) {
