d39767d1fe87ecae36008d1799b2fb808d7b6c3d
Shannen Wu <shannen@mit.edu>
2019-01-15 20:19:00 -0500

    working button in home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../css/homepage.css";
 import "../css/app.css";
+import GameTitle from "./GameTitle";
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <div className="center">
-        MY NAME IS HOME PAGE
+        <GameTitle />
+        <div className="button" onClick={this.props.onClickStart}>Start</div>
       </div>
     );
   }
