53d9b6166932862a4276a58b0d9a10c01960ae91
Andrew Chen <achen88@mit.edu>
2019-01-17 23:12:17 -0500

    fixed port

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.get(["/rules"], (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-http.listen((3000), () => {
+http.listen((process.env.PORT || 3000), () => {
   console.log(`Listening on port 3000 and looking in folder ${publicPath}`);
 });
 
